b7244948ff201a4d6f52af81338bc8db6230f5d3
AmarOk <contact@enconn.fr>
2017-06-02 19:15:43 -0400

    Add \abbr{}{} command

--- scripts/install_texlive.sh
+++ scripts/install_texlive.sh
@@ -3,7 +3,7 @@
 # Forked from Zeste de Savoir:
 # https://github.com/zestedesavoir/zds-site/blob/dev/scripts/install_texlive.sh
 
-EXTRA_PACKAGES="xpatch minted fvextra ifplatform xstring framed capt-of menukeys multirow longtable tabu adjustbox collectbox relsize tikz ntheorem catoptions varwidth blindtext cm-super"
+EXTRA_PACKAGES="adjustbox blindtext capt-of catoptions cm-super collectbox framed fvextra glossaries ifplatform longtable menukeys minted multirow ntheorem relsize tabu tikz varwidth xpatch xstring"
 
 if [[ -f $HOME/.texlive/bin/x86_64-linux/tlmgr ]]; then
   echo "Using cached texlive install"
