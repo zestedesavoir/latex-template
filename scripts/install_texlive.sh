abefb80171c809ee8ff7be8036398aee1f6a751d
Karnaj <yoan.geran@hotmail.fr>
2017-06-05 17:37:56 -0400

    Update install_texlive.sh

--- scripts/install_texlive.sh
+++ scripts/install_texlive.sh
@@ -3,7 +3,7 @@
 # Forked from Zeste de Savoir:
 # https://github.com/zestedesavoir/zds-site/blob/dev/scripts/install_texlive.sh
 
-EXTRA_PACKAGES="adjustbox blindtext capt-of catoptions cm-super collectbox framed fvextra glossaries ifplatform menukeys minted multirow ntheorem relsize tabu varwidth xpatch xstring mfirstuc xfor datatool substr"
+EXTRA_PACKAGES="adjustbox blindtext capt-of catoptions cm-super collectbox framed fvextra glossaries ifplatform menukeys minted multirow ntheorem relsize tabu varwidth xpatch xstring mfirstuc xfor datatool substr tracklang"
 
 if [[ -f $HOME/.texlive/bin/x86_64-linux/tlmgr ]]; then
   echo "Using cached texlive install"
