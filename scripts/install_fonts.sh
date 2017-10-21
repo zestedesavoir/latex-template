ab3318d3ed0cc17d94bf84cd64de31398334d67e
Heziode <Heziode@protonmail.com>
2017-10-21 19:30:29 +0200

    laf/cover: Remove useless file

--- scripts/install_fonts.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# https://fonts.google.com/download?family=Merriweather
-cd /tmp
-mkdir Merriweather
-cd Merriweather
-
-wget https://fonts.google.com/download?family=Merriweather
-unzip Merriweather
