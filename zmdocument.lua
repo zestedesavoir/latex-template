86b339b920357b1e9f4871d08d270093858618b4
Heziode <Heziode@protonmail.com>
2017-12-15 00:49:55 +0100

    fix/author: Improve lua script
    Put URL in global variable instead of passing it in each required functions.

--- zmdocument.lua
+++ zmdocument.lua
@@ -1,11 +1,11 @@
 utf8 = require "./utf8.lua/utf8.lua"
 
-AUTHOR_MAX_SIZE = 15
+AUTHOR_MAX_SIZE    = 15
 AUTHOR_MAX_DISPLAY = 11
-
-array_authors = {}
-array_authors_size = 0
-numberOfColumns = 1
+AUTHOR_LINK        = ""
+ARRAY_AUTHORS      = {}
+ARRAY_AUTHORS_SIZE = 0
+NUMBER_OF_COLUMNS  = 1
 
 --  Split and Join a string into / from a table
 --  source: http://www.wellho.net/resources/ex.php4?item=u105/spjo
@@ -22,11 +22,11 @@ function implode (delimiter, list)
 end
 
 --  Create array of authors which contains for all author
---  - name : default name
---  - display_name : short name (less than AUTHOR_MAX_SIZE) that will be displayed
+--  - name: default name
+--  - display_name: short name (less than AUTHOR_MAX_SIZE) that will be displayed
 function makeAuthorsArray (authors)
-   array_authors = {}
-   local index = -1
+   ARRAY_AUTHORS = {}
+   local index   = -1
    local tmp_name
 
    for word in string.gmatch (authors, '([^,]+)') do
@@ -34,29 +34,29 @@ function makeAuthorsArray (authors)
 
       --  Trim
       tmp_name = string.format ("%s", word:match ("%s*(.+)%s*"))
-      array_authors[index] = { name = tmp_name, display_name = tmp_name }
+      ARRAY_AUTHORS[index] = { name = tmp_name, display_name = tmp_name }
 
       --  Count number of backslash in string to remove it from the count of the size
-      --  Note: this method no cover all backslash aspect (like command), it is just for simple char
+      --  Note: this method no cover all backslash aspect (like command), it is just for simple escaped char
       local numberOfBackslash = 0
-      for i in string.gmatch (array_authors[index].display_name, "\\") do
+      for i in string.gmatch (ARRAY_AUTHORS[index].display_name, "\\") do
          numberOfBackslash = numberOfBackslash + 1
       end
 
       --  Limit size of display name to AUTHOR_MAX_SIZE
-      if (utf8.len (array_authors[index].display_name) - numberOfBackslash) > AUTHOR_MAX_SIZE then
-         array_authors[index].display_name = string.sub (array_authors[index].display_name, 0, AUTHOR_MAX_SIZE - 3) .. "..."
+      if (utf8.len (ARRAY_AUTHORS[index].display_name) - numberOfBackslash) > AUTHOR_MAX_SIZE then
+         ARRAY_AUTHORS[index].display_name = string.sub (ARRAY_AUTHORS[index].display_name, 0, AUTHOR_MAX_SIZE - 3) .. "..."
       end
    end
 
-   array_authors_size = index
+   ARRAY_AUTHORS_SIZE = index
 end
 
 --  Display athors
 --  If "all" parameter is not nil, this will display all authors
-function displayAuthors (link, all)
-   all = all or false
-   local index = array_authors_size
+function displayAuthors (all)
+   all         = all or false
+   local index = ARRAY_AUTHORS_SIZE
 
    if index > AUTHOR_MAX_DISPLAY and not all then
       index = AUTHOR_MAX_DISPLAY
@@ -65,14 +65,14 @@ function displayAuthors (link, all)
    local tab = {}
 
    for i = 0, index do
-      tab[i] = "\\href{" .. link .. "/" .. array_authors[i].name .. "/}{\\color{titlePageAuthorColor}\\bsc{" .. array_authors[i].display_name .. "}}\\\\"
+      tab[i] = "\\href{" .. AUTHOR_LINK .. "/" .. ARRAY_AUTHORS[i].name .. "/}{\\color{titlePageAuthorColor}\\bsc{" .. ARRAY_AUTHORS[i].display_name .. "}}\\\\"
    end
 
    local strAuthors = implode (",", tab)
 
-   if array_authors_size > AUTHOR_MAX_DISPLAY and not all then
+   if ARRAY_AUTHORS_SIZE > AUTHOR_MAX_DISPLAY and not all then
       strAuthors = strAuthors .. ", \\hyperlink{authorsList}{\\color{titlePageAuthorColor}\\bsc{et " ..
-         (array_authors_size - AUTHOR_MAX_DISPLAY) .. " autre(s) auteur(s)}}\\\\"
+         (ARRAY_AUTHORS_SIZE - AUTHOR_MAX_DISPLAY) .. " autre(s) auteur(s)}}\\\\"
    end
 
    print (strAuthors)
@@ -82,47 +82,47 @@ end
 
 --  Return the number of authors can be displayed (lower or equal to AUTHOR_MAX_DISPLAY)
 function getAuthorsNumberMaxDisplayed ()
-   if array_authors_size > AUTHOR_MAX_DISPLAY then
+   if ARRAY_AUTHORS_SIZE > AUTHOR_MAX_DISPLAY then
       return AUTHOR_MAX_DISPLAY
    else
-      return array_authors_size
+      return ARRAY_AUTHORS_SIZE
    end
 end
 
 --  Format authors for displaying on cover
 function formatAuthors (authors, link)
    makeAuthorsArray (authors)
+   AUTHOR_LINK = link
    local authorsByColumn = 5
    local authorsNumber = getAuthorsNumberMaxDisplayed ()
 
-   numberOfColumns = math.ceil (authorsNumber / authorsByColumn)
+   NUMBER_OF_COLUMNS = math.ceil (authorsNumber / authorsByColumn)
 
-   if numberOfColumns > 1 then
-      tex.print ("\\begin{multicols}{" .. numberOfColumns .. "}")
+   if NUMBER_OF_COLUMNS > 1 then
+      tex.print ("\\begin{multicols}{" .. NUMBER_OF_COLUMNS .. "}")
    end
 
-   displayAuthors (link)
+   displayAuthors ()
 
-   if numberOfColumns > 1 then
+   if NUMBER_OF_COLUMNS > 1 then
       tex.print ("\\end{multicols}")
    end
 end
 
 --  Create page for display all authors, only if all authors are not displayed on cover
-function makeAuthorsPage (link)
-
-   if array_authors_size <= AUTHOR_MAX_DISPLAY then
+function makeAuthorsPage ()
+   if ARRAY_AUTHORS_SIZE <= AUTHOR_MAX_DISPLAY then
       --  Displaying this page only if all authors are not displayed on cover
       return
    end
 
-   if numberOfColumns > 1 then
-      tex.print ("\\begin{center}\\section*{\\hypertarget{authorsList}{Auteurs}}\\end{center}\\begin{multicols}{" .. numberOfColumns .. "}")
+   if NUMBER_OF_COLUMNS > 1 then
+      tex.print ("\\begin{center}\\section*{\\hypertarget{authorsList}{Auteurs}}\\end{center}\\begin{multicols}{" .. NUMBER_OF_COLUMNS .. "}")
    end
 
-   displayAuthors(link, true)
+   displayAuthors(true)
 
-   if numberOfColumns > 1 then
+   if NUMBER_OF_COLUMNS > 1 then
       tex.print ("\\end{multicols}\\newpage")
    end
 end
