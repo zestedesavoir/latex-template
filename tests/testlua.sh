561a16713813e5828c0ffe6ab87d9b5a6d1309fe
Pierre Beaujean <pierreb24@gmail.com>
2018-11-18 13:23:08 +0100

    where did it go wrong?

--- tests/testlua.sh
+++ tests/testlua.sh
@@ -2,6 +2,8 @@
 expected=$(cat ./testlua.lua.expected)
 actual=$(texlua ./testlua.lua)
 if [[ "$expected" != "$actual" ]]; then 
+    echo $expected
+    echo $actual
     echo '!! The output of `texlua testlua.lua` changed: zmdocument.lua was modified.'
     echo '!! Did you forget to do `texlua ./testlua.lua > testlua.lua.expected` ?'
     exit 1
